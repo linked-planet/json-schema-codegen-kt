f466cc369b6c3d16cb793685b5c19e1e8cf3452d
Patrick Louis <patrick.louis@link-time.com>
2021-10-12 10:02:50 +0200

    Changed build to produce runnable jar-file instead of library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "1.5.10"
+    application
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "com.linked-planet.tools."
@@ -12,4 +16,16 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.google.code.gson:gson:2.8.8")
+}
+
+application {
+    mainClassName =  "com.linkedplanet.tools.jsonschemakt.Application"
+}
+
+tasks.withType<ShadowJar> {
+    archiveBaseName.set(project.name)
+    archiveClassifier.set("all")
+    transform(com.github.jengelman.gradle.plugins.shadow.transformers.AppendingTransformer::class.java) {
+        resource = "reference.conf"
+    }
 }
--- src/main/kotlin/com/linkedplanet/tools/jsonschemakt/Application.kt
+++ src/main/kotlin/com/linkedplanet/tools/jsonschemakt/Application.kt
@@ -5,46 +5,51 @@ import com.linkedplanet.tools.jsonschemakt.model.Schema
 import com.linkedplanet.tools.jsonschemakt.templates.*
 import java.io.File
 
-fun main(args: Array<String>) {
-    val arguments = parseArgs(args.toList())
-    arguments.sourceFiles?.onEach { fileName ->
-        File("${arguments.sourceFolder ?: ""}$fileName").process(arguments.targetPath)
-    } ?: arguments.sourceFolder?.let { folderName ->
-        File(folderName).walk()
-            .filter { it.isFile && it.name.endsWith(".json") }
-            .forEach { it.process(arguments.targetPath) }
-    }
-}
-
-fun File.process(targetPath: String?): Unit {
-    val reader = inputStream().reader()
-    val result = Gson().fromJson(reader, Schema::class.java)
-    val dir = File("${targetPath ?: ""}/${result.id.toPackage().replace(".", "/")}")
-    dir.mkdirs()
-    val oFile = File("${dir.path}/${result.title}.kt").outputStream()
-    oFile.write(schemaTemplate(result).toByteArray())
-}
-
-fun parseArgs(args: List<String>): AppArgs =
-    if (args.isNotEmpty()) {
-        when (args[0]) {
-            "-t" -> AppArgs(args[1], null, null) + parseArgs(args.drop(2))
-            "-s" -> AppArgs(null, args[1], null) + parseArgs(args.drop(2))
-            else -> AppArgs(null, null, args)
+class Application {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val arguments = parseArgs(args.toList())
+            arguments.sourceFiles?.onEach { fileName ->
+                File("${arguments.sourceFolder ?: ""}$fileName").process(arguments.targetPath)
+            } ?: arguments.sourceFolder?.let { folderName ->
+                File(folderName).walk()
+                    .filter { it.isFile && it.name.endsWith(".json") }
+                    .forEach { it.process(arguments.targetPath) }
+            }
         }
-    } else {
-        AppArgs(null, null, null)
-    }
 
-data class AppArgs(
-    val targetPath: String?,
-    val sourceFolder: String?,
-    val sourceFiles: List<String>?
-) {
-    operator fun plus(b: AppArgs): AppArgs =
-        AppArgs(
-            targetPath ?: b.targetPath,
-            sourceFolder ?: b.sourceFolder,
-            sourceFiles ?: b.sourceFiles
-        )
+        fun File.process(targetPath: String?): Unit {
+            val reader = inputStream().reader()
+            val result = Gson().fromJson(reader, Schema::class.java)
+            val dir = File("${targetPath ?: ""}/${result.id.toPackage().replace(".", "/")}")
+            dir.mkdirs()
+            val oFile = File("${dir.path}/${result.title}.kt").outputStream()
+            oFile.write(schemaTemplate(result).toByteArray())
+        }
+
+        fun parseArgs(args: List<String>): AppArgs =
+            if (args.isNotEmpty()) {
+                when (args[0]) {
+                    "-t" -> AppArgs(args[1], null, null) + parseArgs(args.drop(2))
+                    "-s" -> AppArgs(null, args[1], null) + parseArgs(args.drop(2))
+                    else -> AppArgs(null, null, args)
+                }
+            } else {
+                AppArgs(null, null, null)
+            }
+
+        data class AppArgs(
+            val targetPath: String?,
+            val sourceFolder: String?,
+            val sourceFiles: List<String>?
+        ) {
+            operator fun plus(b: AppArgs): AppArgs =
+                AppArgs(
+                    targetPath ?: b.targetPath,
+                    sourceFolder ?: b.sourceFolder,
+                    sourceFiles ?: b.sourceFiles
+                )
+        }
+    }
 }
