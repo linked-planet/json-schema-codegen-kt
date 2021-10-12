7704fb2dda7ec0bb9790150e8c002a47d8446bde
Patrick Louis <patrick.louis@link-time.com>
2021-10-12 11:02:08 +0200

    Implemented generation of max-size checking
    Generates a function `checkSizeConstraints` on Object types that will,
    given an object of its type check whether all the fields match their
    respective max-size -- if given.

--- src/main/kotlin/com/linkedplanet/tools/jsonschemakt/templates/Object.kt
+++ src/main/kotlin/com/linkedplanet/tools/jsonschemakt/templates/Object.kt
@@ -9,10 +9,30 @@ ${baseIndent}data class ${name.capitalize()}(
 ${properties.map { (propName, prop) ->
     "${baseIndent}${indent(1)}val $propName: ${prop.propTypeToKotlinType(propName)}${if(!required.contains(propName)) { "?" } else { "" } }"
 }.joinToString(",\n","","")}
-${baseIndent})
+${baseIndent}) {
+${baseIndent}${indent(1)}companion object {
+${baseIndent}${indent(2)}const val sizeConstraints = mapOf(
+${properties.mapNotNull { (k, p) -> p.generateSizeConstraintEntry(baseIndent + indent(3), name, k) }.joinToString(",\n") }
+${baseIndent}${indent(2)})
+${baseIndent}${indent(1)}}
+${generateSizeConstraintFunction(baseIndent + indent(1), name)}
+${baseIndent}}
 
 ${baseIndent}${properties.generateChildTypes(baseIndent).joinToString("\n\n","","")}
 """.trimIndent()
+
+@Language("kotlin")
+fun generateSizeConstraintFunction(indent: String, clsName: String): String =
+    """
+${indent}fun checkSizeConstraints(): Boolean =
+${indent + indent(1)}${clsName}.sizeConstraints.all { (prop, size) ->
+${indent + indent(2)}prop.getter.call(this).sizeOf() <= size
+${indent + indent(1)}}
+    """
+
+@Language("kotlin")
+fun Property.generateSizeConstraintEntry(indent: String, clsName: String, propName: String): String? =
+    maxLength?.let { "${indent}${clsName}::${propName} to $it" }
 
 fun Property.propTypeToKotlinType(name: String): String =
     when(this.type) {
